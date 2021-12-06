d30b74fb34f24ababf4f65de91bc19ba34f93d3f
Gagenth CHANDRASEGARAM <c.gagenth@gmail.com>
2021-12-06 20:24:54 +0100

    Add Composite Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,13 +1,12 @@
 package fr.lernejo.logger;
-
 public class LoggerFactory implements Logger{
 
-        public static Logger getLogger( String name ) {
-            return new ContextualLogger( name, new FileLogger("./runLogs.log") );
-        }
-
-        @Override
-        public void log(String message) {
-            System.out.println(message);
-        }
+    public static Logger getLogger( String name ) {
+        return new ContextualLogger( name, new CompositeLogger( new ConsoleLogger(), new FileLogger( "./runLogs.log" ) ) );
     }
+
+    @Override
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
