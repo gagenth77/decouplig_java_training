67b78a85778393a20a0c1db415e3915d527f39c3
Gagenth CHANDRASEGARAM <c.gagenth@gmail.com>
2021-12-06 20:09:32 +0100

    Add File+Contextual Logger+File Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,13 +2,12 @@ package fr.lernejo.logger;
 
 public class LoggerFactory implements Logger{
 
-    public static Logger getLogger(String name){
+        public static Logger getLogger( String name ) {
+            return new ContextualLogger( name, new FileLogger("./runLogs.log") );
+        }
 
-        return new ConsoleLogger();
+        @Override
+        public void log(String message) {
+            System.out.println(message);
+        }
     }
-
-    @Override
-    public void log(String message) {
-        System.out.println(message);
-    }
-}
